2f16cc312b857d82486abc1c94d04292fa6d1329
Evan Neill <magikarp@Evans-MacBook-Pro.local>
2020-03-20 18:03:42 +0000

    trying to make config work in colab...

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 # This workaround is to be able to look at data in your google drive on colab rather than uploading data to a colab directory every time
 if os.environ.get('CLOUDSDK_CONFIG') is not None:	
-	dataDir = "drive/My Drive/data/"
+	DATA_DIR = "/content/drive/My Drive/data/"
 else:
-	dataDir = "data/"
+	DATA_DIR = os.path.abspath('data/')
 
-DATA_DIR = os.path.abspath(dataDir)
