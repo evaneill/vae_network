7aefbb6e9bd3e57bb8636ce8012a25bc4f62b06f
Evan Neill <magikarp@Evans-MacBook-Pro.local>
2020-03-20 17:54:39 +0000

    change to config to allow for colab to automatically reference drive/My Drive/data/ instead of <repo>/data

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
-dataDir = "data/"
+# This workaround is to be able to look at data in your google drive on colab rather than uploading data to a colab directory every time
+if os.environ.get('CLOUDSDK_CONFIG') is not None:	
+	dataDir = "drive/My Drive/data/"
+else:
+	dataDir = "data/"
 
 DATA_DIR = os.path.abspath(dataDir)
