2302005d39bba978d8ea3c1c0b67348cbd120393
Evan Neill <magikarp@Evans-MacBook-Pro.local>
2020-03-21 14:49:22 +0000

    update train function with latest failures

--- train.py
+++ train.py
@@ -11,12 +11,12 @@ from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 from torchvision.utils import save_image
 
-def main(data,model,alpha,optimize_on,batch_size,learning_rate,n_epochs,imsize,eval_log_marginal=True,log_interval=10):
+def main(data,model,alpha,optimize_on,batch_size,learning_rate,n_epochs,imsize,test_batch_size,eval_log_marginal=True,log_interval=10):
 
 	data_train, data_test = data[0], data[1]
 
 	data_train_loader = DataLoader(TensorDataset(data_train),batch_size = batch_size)
-	data_test_loader = DataLoader(TensorDataset(data_test),batch_size = batch_size)
+	data_test_loader = DataLoader(TensorDataset(data_test),batch_size = test_batch_size)
 
 	if torch.cuda.is_available():
 		device = torch.device('cuda')
@@ -89,17 +89,23 @@ def test(epoch,model,optimizer,alpha,data_loader,optimize_on,device,imsize,n_sam
 	print('====> Test set loss: {:.4f}'.format(test_loss))
 	return test_loss 
     
-def score(model,alpha,test_data_loader,device,n_samples=5000):
-	score = 0
-	model.eval()
-	for [batch] in iter(test_data_loader):
-		batch = batch.to(device)
-		output, q_mu, q_log_sigma = model.forward(batch,n_samples = n_samples)
-		batch_score = -1*VRBound(alpha,model,output,q_mu, q_log_sigma,optimize_on='full_lowerbound')
-		score+=batch_score 
+def score(model,test_data_loader,device,n_samples=5000):
+	if n_samples is None:
+		n_samples = model.encoder.n_samples
+	elif type(n_samples)!=int:
+		raise Exception
 
-	score/= len(data_loader.dataset)
-	return score# I think I want to divide? Printed output from above seems to be the thing I want
+	model.eval()
+	test_loss = 0
+	with torch.no_grad():
+	    for i, [data] in enumerate(test_data_loader):
+	        data = data.to(device)
+	        output, q_mu, q_log_sigma = model.forward(data,n_samples = n_samples)
+	        test_loss += -1*VRBound(0,model,output,q_mu, q_log_sigma,optimize_on='full_lowerbound')
+
+	test_loss /= len(test_data_loader.dataset)*n_samples
+	print('====> Test set loss: {:.4f}'.format(test_loss))
+	return test_loss # I think I want to divide? Printed output from above seems to be the thing I want
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description='try to train a model!')
@@ -114,6 +120,7 @@ if __name__=="__main__":
 	parser.add_argument('--alpha','-a',type=float,default=1.)#,description='alpha of Renyi Alpha-Divergence used in deriving bound')
 	parser.add_argument('--n_samples','-k',type=int,default=5)#,description='number of samples drawn per input observation')
 	parser.add_argument('--batch_size','-batch',type=int,default=100)#,description='batch size of training passes (before sampling)')
+	parser.add_argument('--test_batch','-testbatch',type=int,default=10) # batch size for evaluation of runtime test error and K=5000 test
 	parser.add_argument('--optimize_on',choices=['full_lowerbound','max'],default='full_lowerbound')#,description='whether a full sum should be taken, or only the max-weight observation when estimating the bound')
 	parser.add_argument('--learning_rate','-lr',type=float,default=-1.)
 	parser.add_argument('--n_epoch','-e',type=int,default=100)
@@ -128,6 +135,7 @@ if __name__=="__main__":
 	alpha = args.alpha
 	n_samples = args.n_samples
 	batch_size = args.batch_size
+	test_batch_size = args.test_batch_size
 	optimize_on = args.optimize_on
 	learning_rate = args.learning_rate
 	n_epochs = args.n_epoch
@@ -161,4 +169,4 @@ if __name__=="__main__":
 	elif L==2:
 		model = VRalphaNet(data[0],[(200,'d'),(200,'d'),(100,'s'),(100,'d'),(100,'d'),(50,'s')],'tanh',data_type=data_type,n_samples=n_samples)
 
-	main(data,model,alpha,optimize_on,batch_size,learning_rate,n_epochs,imsize,log_interval=log_interval)
+	main(data,model,alpha,optimize_on,batch_size,learning_rate,n_epochs,imsize,test_batch_size,log_interval=log_interval)
