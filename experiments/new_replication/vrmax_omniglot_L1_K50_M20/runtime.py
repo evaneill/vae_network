2c3b0ef45ea086069f966162e998da603667dd4f
Arne Gebert <35544567+arnegebert@users.noreply.github.com>
2020-04-21 19:30:36 +0200

    Same bugfixes, different script

--- experiments/new_replication/vrmax_omniglot_L1_K50_M20/runtime.py
+++ experiments/new_replication/vrmax_omniglot_L1_K50_M20/runtime.py
@@ -22,7 +22,7 @@ for r in range(num_rounds+1):
     print(f"====== About to train for {3**r} epochs in round {r} with learning rate {round(current_round_lr,7)}========")
     logging.info(f"====== About to train for {3**r} epochs in round {r} with learning rate {round(current_round_lr,7)}========")
     for epoch in range(3**r):
-        train(r,epoch)
+        train(r,epoch, optimizer)
     with open(f'{model_type}_{data_name}_K{K}_M{batch_size}/{model_type}_{data_name}_K{K}_M{batch_size}_LR0001.pt','wb') as f:
         print(datetime.datetime.now())
         logging.info(datetime.datetime.now())
@@ -31,8 +31,8 @@ for r in range(num_rounds+1):
     
     _test(r,epoch)
     with torch.no_grad():
-        sample = torch.randn(64, 200).to(device)
-        sample = model.decode(sample,test=True).cpu()
+        sample = torch.randn(64, 50).to(device)
+        sample = model.decode(sample).cpu()
         save_image(sample.view(64, 1, 28, 28),
                     f'{model_type}_{data_name}_K{K}_M{batch_size}/samples/sample_' +str(r)+'_'+ str(epoch) + '.png')
                 
