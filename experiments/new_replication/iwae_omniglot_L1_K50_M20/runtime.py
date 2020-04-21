a77443e19ac90b650b38c4e4708e770e8d31e1f9
Arne Gebert <35544567+arnegebert@users.noreply.github.com>
2020-04-21 19:14:57 +0200

    Wrong keyword argument

--- experiments/new_replication/iwae_omniglot_L1_K50_M20/runtime.py
+++ experiments/new_replication/iwae_omniglot_L1_K50_M20/runtime.py
@@ -11,6 +11,7 @@ if torch.cuda.is_available():
     logging.info("Training on GPU")
 
 os.makedirs(f'{model_type}_{data_name}_K{K}_M{batch_size}',exist_ok=True)
+os.makedirs(f'{model_type}_{data_name}_K{K}_M{batch_size}/results',exist_ok=True)
 os.makedirs(f'{model_type}_{data_name}_K{K}_M{batch_size}/samples',exist_ok=True)
 os.makedirs(f'{model_type}_{data_name}_K{K}_M{batch_size}/recons',exist_ok=True)
 
@@ -32,7 +33,7 @@ for r in range(num_rounds+1):
     _test(r,epoch)
     with torch.no_grad():
         sample = torch.randn(64, 200).to(device)
-        sample = model.decode(sample,test=True).cpu()
+        sample = model.decode(sample).cpu()
         save_image(sample.view(64, 1, 28, 28),
                     f'{model_type}_{data_name}_K{K}_M{batch_size}/samples/sample_' +str(r)+'_'+ str(epoch) + '.png')
                 
