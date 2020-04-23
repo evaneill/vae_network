92d8af873dc51b305a3b16a4c6b9a9b0a7daad7b
ArneGeb <35544567+ArneGeb@users.noreply.github.com>
2020-04-23 23:19:38 +0200

    Little change to preserve my sanity
    no more unneccessary printing of epoche training results

--- experiments/fashion/vrmax_fashion_L2_K50_M128/train_and_test.py
+++ experiments/fashion/vrmax_fashion_L2_K50_M128/train_and_test.py
@@ -12,15 +12,7 @@ def train(epoch):
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-            logging.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
+
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch,  train_loss / len(train_loader.dataset)))
