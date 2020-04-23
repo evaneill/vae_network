4ed1d6e2cbf195844d2845698b04946b76ba292e
ArneGeb <35544567+ArneGeb@users.noreply.github.com>
2020-04-23 22:44:23 +0200

    FashionMNIST ... feels dirty..
    128 new files for literally 2 line changes ;__;

---
+++ experiments/fashion/vae_mnist_L2_K5_M128/train_and_hyperparameters.py
@@ -0,0 +1,18 @@
+batch_size = 128
+epochs = 101
+seed = 1
+log_interval = 1000
+testing_frequency = 20
+K = 5
+learning_rate = 1e-3
+discrete_data = True
+alpha = 1
+cuda = torch.cuda.is_available()
+test_batch_size = 32
+model_type = 'vae'
+torch.manual_seed(seed)
+
+data_name = 'mnist' 
+
+logging_filename = f'{model_type}_{data_name}_K{K}_M{batch_size}.log'
+logging.basicConfig(filename=logging_filename,level=logging.DEBUG)
