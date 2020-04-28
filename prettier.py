6657248ab19aae9da6e06952e29f6bdf551a71ee
ArneGeb <35544567+ArneGeb@users.noreply.github.com>
2020-04-28 23:27:08 +0200

    Further improved prettier version

--- prettier.py
+++ prettier.py
@@ -1,23 +1,15 @@
-from __future__ import print_function
-import argparse
+import datetime
+import logging
+import os
+
+import numpy as np
 import torch
 import torch.utils.data
 from torch import nn, optim, Tensor as T
-from torch.nn import functional as F
+from torch.autograd import detect_anomaly
+from torch.distributions.multinomial import Multinomial
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-from torch.autograd import Variable, detect_anomaly
-from torch.distributions.multinomial import Multinomial
-import datetime
-import os
-import pickle
-import numpy as np
-from torch.utils.data import DataLoader, TensorDataset
-from scipy.io import loadmat
-import logging
-import math
-
-from utils import Loader
 
 os.makedirs('results', exist_ok=True)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,15 +20,16 @@ test_batch_size = 32
 epochs = 501
 seed = 1
 
-log_interval = 200
-log_test_value = 100
+log_interval = 1
+test_interval = 1
 learning_rate = 1e-3
 discrete_data = True
 alpha = -500
 torch.manual_seed(seed)
 L = 1
 model_type = 'iwae'
-data_name = 'mnist'
+# omniglot doesn't work currently
+data_name = 'fashion'
 
 assert (L in [1, 2])
 assert (model_type in ['vae', 'iwae', 'vrmax', 'vralpha', 'general_alpha'])
@@ -137,7 +130,7 @@ class mnist_omniglot_model1(nn.Module):
 
 # Define the model
 class mnist_omniglot_model2(nn.Module):
-    def __init__(self):
+    def __init__(self, alpha):
         super(mnist_omniglot_model2, self).__init__()
 
         self.fc1 = nn.Linear(784, 200)
@@ -163,7 +156,6 @@ class mnist_omniglot_model2(nn.Module):
         self.alpha = alpha
 
     def encode(self, x):
-        # h1 = F.relu(self.fc1(x))
         h1 = torch.tanh(self.fc1(x))
         h2 = torch.tanh(self.fc2(h1))
         mu, log_std = self.fc31(h2), self.fc32(h2)
@@ -183,7 +175,6 @@ class mnist_omniglot_model2(nn.Module):
         return mu + eps * std
 
     def decode(self, z, test=False):
-        # h3 = F.relu(self.fc3(z))
         h5 = torch.tanh(self.fc7(z))
         h6 = torch.tanh(self.fc8(h5))
         mu, log_std = self.fc81(h6), self.fc82(h6)
@@ -241,9 +232,14 @@ class mnist_omniglot_model2(nn.Module):
         h8 = torch.tanh(self.fc10(h7))
 
         decoded = torch.sigmoid(self.fc11(h8))
-
         # log p(x | h1)
-        log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
+        if discrete_data:
+            log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
+        else:
+            # Gaussian where sigma = 0, not letting sigma be predicted atm
+            log_px_h1 = compute_log_probabitility_gaussian(decoded, x, torch.zeros_like(decoded))
+
+        # log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
 
         # hopefully this reshape operation magically works like always
         if model_type == 'iwae' or test == True:
@@ -474,9 +470,9 @@ def train(epoch):
         train_loss += loss.item()
         optimizer.step()
 
-    # if epoch % log_interval == 0:
-    print(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
-    logging.info(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
+    if epoch % log_interval == 0:
+        print(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
+        logging.info(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
 
 # pycharm thinks that I want to run a test whenever I define a function that has 'test' as prefix
 # this messes with running the model and is the reason why the function is called _test
@@ -494,33 +490,34 @@ def _test(epoch):
                 comparison = torch.cat([data[:n],
                                       recon_batch.view(test_batch_size, 1, 28, 28)[:n]])
                 save_image(comparison.cpu(),
-                         f'results/reconstruction_{epoch}.png', nrow=n)
+                         f'results/reconstruction_{model_type}_L={L}_{data_name}_alpha={alpha}_K={K}_epoch={epoch}.png',
+                           nrow=n)
+                noise = torch.randn(64, 50).to(device)
+                sample = model.decode(noise).cpu()
+                save_image(sample.view(64, 1, 28, 28),
+                           f'results/sample_{model_type}_L={L}_{data_name}_alpha={alpha}_K={K}_epoch={epoch}.png')
     test_loss /= len(test_loader.dataset)
-    print(f'====> Test set loss: {test_loss:.4f}')
-    logging.info(f'====> Test set loss: {test_loss:.4f}')
+    print(f'====> Epoch: {epoch} Test set loss: {test_loss:.4f}')
+    logging.info(f'====> Epoch: {epoch} Test set loss: {test_loss:.4f}')
     return test_loss
 
 def load_data(data_name, train_batch, test_batch):
     data_name = data_name.lower()
     assert(data_name in ['mnist', 'fashion', 'fashionmnist', 'omniglot'])
     kwargs = {'num_workers': 1, 'pin_memory': True}
+
     if data_name == 'mnist':
-        train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('../data', train=True, download=True,
-                           transform=transforms.ToTensor()),
-            batch_size=train_batch, shuffle=True, **kwargs)
-        test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-            batch_size=test_batch, shuffle=True, **kwargs)
+        train_data = datasets.MNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+        test_data = datasets.MNIST('./data', train=False, transform=transforms.ToTensor())
     elif data_name == 'fashion' or data_name == 'fashionmnist':
-        train_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('../data', train=True, download=True,
-                           transform=transforms.ToTensor()),
-            batch_size=train_batch, shuffle=True, **kwargs)
-        test_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('../data', train=False, transform=transforms.ToTensor()),
-            batch_size=test_batch, shuffle=True, **kwargs)
+        train_data = datasets.FashionMNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+        test_data = datasets.FashionMNIST('./data', train=False, transform=transforms.ToTensor())
+    elif data_name == 'omniglot':
+        train_data = datasets.Omniglot(root='./data', background=True, download=True, transform=transforms.ToTensor())
+        test_data = datasets.Omniglot(root='./data', background=False, download=True, transform=transforms.ToTensor())
     # else: raise Exception("Data name not recognized")
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size = train_batch, shuffle = True, ** kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size = test_batch, shuffle = True, ** kwargs)
     return train_loader, test_loader
 
 if __name__ == "__main__":
@@ -535,11 +532,12 @@ if __name__ == "__main__":
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    print(datetime.datetime.now())
-    logging.info(datetime.datetime.now())
-    _test(0)
-    for e in range(epochs):
+    print(f'{datetime.datetime.now()} \nStarting training')
+    logging.info(f'{datetime.datetime.now()} \nStarting training')
+    for e in range(1, epochs+1):
         train(e)
+        if e % test_interval == 0:
+            _test(e)
     _test(epochs)
     print(datetime.datetime.now())
     logging.info(datetime.datetime.now())
