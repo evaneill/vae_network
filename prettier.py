3efe0f290d6ab386cd509cf6a37673fef4669b5b
Evan Neill <magikarp@Evans-MacBook-Pro.local>
2020-04-29 13:47:26 +0100

    added a LOT of comments. removed some extraneous code bits.

--- prettier.py
+++ prettier.py
@@ -63,6 +63,7 @@ class mnist_omniglot_model1(nn.Module):
     def reparameterize(self, mu, logstd):
         std = torch.exp(logstd)
         eps = torch.randn_like(std)
+        # This is the reparametrization trick - represent the sample as a sum rather than black-box generated number
         return mu + eps*std
 
     def decode(self, z):
@@ -78,46 +79,79 @@ class mnist_omniglot_model1(nn.Module):
     def compute_loss_for_batch(self, data, model, K=K, test=False):
         # data = (B, 1, H, W)
         B, _, H, W = data.shape
+
+        # Generate K copies of each observation. Each will get sampled once according to the generated distribution to generate a total of K observation samples
         data_k_vec = data.repeat((1, K, 1, 1)).view(-1, H*W)
+
+        # Retrieve the estimated mean and log(standard deviation) estimates from the posterior approximator
         mu, logstd = model.encode(data_k_vec)
-        # (B*K, #latents)
+
+        # Use the reparametrization trick to generate (mean)+(epsilon)*(standard deviation) for each sample of each observation
         z = model.reparameterize(mu, logstd)
 
-        # summing over latents due to independence assumption
-        # (B*K)
+        # Calculate log q(z|x) - how likely are the importance samples given the distribution that generated them?
         log_q = compute_log_probabitility_gaussian(z, mu, logstd)
 
+        # Calculate log p(z) - how likely are the importance samples under the prior N(0,1) assumption?
         log_p_z = compute_log_probabitility_gaussian(z, torch.zeros_like(z, requires_grad=False), torch.zeros_like(z, requires_grad=False))
+        
+        # Hand the samples to the decoder network and get a reconstruction of each sample.
         decoded = model.decode(z)
-        if discrete_data:
-            log_p = compute_log_probabitility_bernoulli(decoded, data_k_vec)
-        else:
-            # Gaussian where sigma = 0, not letting sigma be predicted atm
-            log_p = compute_log_probabitility_gaussian(decoded, data_k_vec, torch.zeros_like(decoded))
-        # hopefully this reshape operation magically works like always
+
+        # Calculate log p(x|z) with a bernoulli distribution - how likely are the recreations given the latents that generated them?
+        log_p = compute_log_probabitility_bernoulli(decoded, data_k_vec)
+
+        # Begin calculating L_alpha depending on the (a) model type, and (b) optimization method
+        # log_p_z + log_p - log_q = log(p(z_i)p(x|z_i)/q(z_i|x)) = log(p(x,z_i)/q(z_i|x)) = L_VI
+        #   (for each importance sample i out of K for each observation)
         if model_type == 'iwae' or test:
+            # Re-order the entries so that each row holds the K importance samples for each observation
             log_w_matrix = (log_p_z + log_p - log_q).view(B, K)
+        
         elif model_type =='vae':
-            # treat each sample for a given data point as you would treat all samples in the minibatch
-            # 1/K value because loss values seemed off otherwise
+            # Don't reorder, and divide by K in anticipation of taking a batch sum of (1/K)*SUM(log(p(x,z)/q(z|x)))
             log_w_matrix = (log_p_z + log_p - log_q).view(B*K, 1)*1/K
+        
         elif model_type=='general_alpha' or model_type=='vralpha':
+            # Re-order the entries so that each row holds the K importance samples for each observation
+            # Multiply by (1-alpha) because (1-alpha)* log(p(x,z_i)/q(z_i|x)) =  log([p(x,z_i)/q(z_i|x)]^(1-alpha))
             log_w_matrix = (log_p_z + log_p - log_q).view(-1, K) * (1-alpha)
+        
         elif model_type == 'vrmax':
+            # Re-order the entries so that each row holds the K importance samples for each observation
+            # Take the max in each row, representing the maximum-weighted sample
             log_w_matrix = (log_p_z + log_p - log_q).view(-1, K).max(axis=1, keepdim=True).values
+
+            # immediately return loss = -sum(L_alpha) over each observation
             return -torch.sum(log_w_matrix)
+
+        # Begin using the "max trick". Subtract the maximum log(*) sample value for each observation.
+        # log_w_minus_max = log([p(z_i,x)/q(z_i|x)] / max([p(z_k,x)/q(z_k|x)]))
         log_w_minus_max = log_w_matrix - torch.max(log_w_matrix, 1, keepdim=True)[0]
+
+        # Exponentiate so that each term is [p(z_i,x)/q(z_i|x)] / max([p(z_k,x)/q(z_k|x)]) (no log)
         ws_matrix = torch.exp(log_w_minus_max)
+
+        # Calculate normalized weights in each row. Max denominators cancel out!
+        # ws_norm = [p(z_i,x)/q(z_i|x)]/SUM([p(z_k,x)/q(z_k|x)])
         ws_norm = ws_matrix / torch.sum(ws_matrix, 1, keepdim=True)
 
         if model_type == 'vralpha' and not test:
+            # If we're specifically using a VR-alpha model, we want to choose a sample to backprop according to the values in ws_norm above
+            # So we make a distribution in each row
             sample_dist = Multinomial(1, ws_norm)
+
+            # Then we choose a sample in each row acccording to this distribution
             ws_sum_per_datapoint = log_w_matrix.gather(1, sample_dist.sample().argmax(1, keepdim=True))
         else:
+            # For any other model, we're taking the full sum at this point
             ws_sum_per_datapoint = torch.sum(log_w_matrix * ws_norm, 1)
 
         if model_type in ["general_alpha", "vralpha"] and not test:
+            # For both VR-alpha and directly estimating L_alpha with a sum, we have to renormalize the sum with 1-alpha
             ws_sum_per_datapoint /= (1 - alpha)
+
+        # Return a value of loss = -L_alpha as the batch sum.
         loss = -torch.sum(ws_sum_per_datapoint)
 
         return loss
@@ -166,6 +200,7 @@ class mnist_omniglot_model2(nn.Module):
             eps = torch.zeros_like(mu)
         else:
             eps = torch.randn_like(std)
+        # This is the reparametrization trick - represent the sample as a sum rather than black-box generated number
         return mu + eps * std
 
     def decode(self, z, test=False):
@@ -186,80 +221,106 @@ class mnist_omniglot_model2(nn.Module):
 
     def compute_loss_for_batch(self, data, model, K=K, test=False):
         B, _, H, W = data.shape
-        data_k_vec = data.repeat((1, K, 1, 1)).view(-1, H * W)
-        # data_k_vec = data.repeat_interleave(K, 0)
 
+        # First repeat the observations K times, representing the data as a flat (M*K, # of pixels)
+        data_k_vec = data.repeat((1, K, 1, 1)).view(-1, H * W)
+
+        # Encode the model and retrieve estimated distribution parameters mu and log(standard deviation) for each sample of each observation
+        # z1 holds the latent samples generated at the first stochastic layer.
         mu, log_std, [x, z1] = self.encode(data_k_vec)
-        # (B*K, #latents)
+
+        # Sample from each observation's approximated latent distribution in each row (i.e. once for each of K importance samples, represented by rows)
+        # (this uses the reparametrization trick!)
         z = model.reparameterize(mu, log_std)
 
-        # Log p(z) (prior)
+        # Calculate Log p(z) (prior) - how likely are these values given the prior assumption N(0,1)?
         log_p_z = torch.sum(-0.5 * z ** 2, 1) - .5 * z.shape[1] * T.log(torch.tensor(2 * np.pi))
 
-        # q (z | h1)
+        # Calculate q (z | h1) - how likely are the generated output latent samples given the distributions they came from?
         log_qz_h1 = compute_log_probabitility_gaussian(z, mu, log_std)
 
+        # Re-Generate the mu and log_std that generated the first-layer latents z1
         h1 = torch.tanh(self.fc1(x))
         h2 = torch.tanh(self.fc2(h1))
         mu, log_std = self.fc31(h2), self.fc32(h2)
 
-        # q (h1 | x)
+        # Calculate log q(h1|x) - how likely are the first-stochastic-layer latents given the distributions they come from?
         log_qh1_x = compute_log_probabitility_gaussian(z1, mu, log_std)
 
+        # Calculate the distribution parameters that generated the first-layer latents upon decoding
         h5 = torch.tanh(self.fc7(z))
         h6 = torch.tanh(self.fc8(h5))
         mu, log_std = self.fc81(h6), self.fc82(h6)
 
-        # log p(h1 | z)
+        # Calculate log p(h1|z) - how likely are the latents z1 under the parameters of the distribution here?
+        #   (This directly encourages the decoder to learn the inverse of the map h1->z)
         log_ph1_z = compute_log_probabitility_gaussian(z1, mu, log_std)
 
+        # Finally calculate the reconstructed image
         h7 = torch.tanh(self.fc9(z1))
         h8 = torch.tanh(self.fc10(h7))
-
         decoded = torch.sigmoid(self.fc11(h8))
-        # log p(x | h1)
-        if discrete_data:
-            log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
-        else:
-            # Gaussian where sigma = 0, not letting sigma be predicted atm
-            log_px_h1 = compute_log_probabitility_gaussian(decoded, x, torch.zeros_like(decoded))
 
-        # log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
+        # calculate log p(x | h1) - how likely is the reconstruction given the latent samples that generated it?
+        log_px_h1 = compute_log_probabitility_bernoulli(decoded, x)
 
-        # hopefully this reshape operation magically works like always
+        # Begin calculating L_alpha depending on the (a) model type, and (b) optimization method
+        # log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x = 
+        #           log([p(z0_i)p(x|z1_i)p(z1_i|z0_i)]/[q(z0_i|z1_i)q(z1_i|x)]) = log(p(x,z0_i,z1_i)/q(z0_i,z1_i|x)) = L_VI
+        #   (for each importance sample i out of K for each observation)
+        # Note that if test==True then we're always using the IWAE objective!
         if model_type == 'iwae' or test == True:
+            # Re-order the entries so that each row holds the K importance samples for each observation
             log_w_matrix = (log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x).view(-1, K)
+
         elif model_type == 'vae':
-            # treat each sample for a given data point as you would treat all samples in the minibatch
-            # 1/K value because loss values seemed off otherwise
+            # Don't reorder, and divide by K in anticipation of taking a batch sum of (1/K)*SUM(log(p(x,z)/q(z|x)))
             log_w_matrix = (log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x).view(-1, 1) * 1 / K
             return -torch.sum(log_w_matrix)
+
         elif model_type == 'general_alpha' or model_type == 'vralpha':
+            # Re-order the entries so that each row holds the K importance samples for each observation
+            # Multiply by (1-alpha) because (1-alpha)* log(p(x,z_i)/q(z_i|x)) =  log([p(x,z_i)/q(z_i|x)]^(1-alpha))
             log_w_matrix = (log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x).view(-1, K) * (1 - self.alpha)
+
         elif model_type == 'vrmax':
-            log_w_matrix = (log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x).view(-1, K).max(axis=1,
-                                                                                                     keepdim=True).values
+            # Re-order the entries so that each row holds the K importance samples for each observation
+            # Take the max in each row, representing the maximum-weighted sample, then immediately return batch sum loss -L_alpha
+            log_w_matrix = (log_p_z + log_ph1_z + log_px_h1 - log_qz_h1 - log_qh1_x).view(-1, K).max(axis=1,keepdim=True).values
             return -torch.sum(log_w_matrix)
 
+        # Begin using the "max trick". Subtract the maximum log(*) sample value for each observation.
+        # log_w_minus_max = log([p(z_i,x)/q(z_i|x)] / max([p(z_k,x)/q(z_k|x)]))
         log_w_minus_max = log_w_matrix - torch.max(log_w_matrix, 1, keepdim=True)[0]
+
+        # Exponentiate so that each term is [p(z_i,x)/q(z_i|x)] / max([p(z_k,x)/q(z_k|x)]) (no log)
         ws_matrix = torch.exp(log_w_minus_max)
+
+        # Calculate normalized weights in each row. Max denominators cancel out!
+        # ws_norm = [p(z_i,x)/q(z_i|x)]/SUM([p(z_k,x)/q(z_k|x)])
         ws_norm = ws_matrix / torch.sum(ws_matrix, 1, keepdim=True)
 
         if model_type == 'vralpha' and not test:
+            # If we're specifically using a VR-alpha model, we want to choose a sample to backprop according to the values in ws_norm above
+            # So we make a distribution in each row
             sample_dist = Multinomial(1, ws_norm)
+
+            # Then we choose a sample in each row acccording to this distribution
             ws_sum_per_datapoint = log_w_matrix.gather(1, sample_dist.sample().argmax(1, keepdim=True))
         else:
+            # For any other model, we're taking the full sum at this point
             ws_sum_per_datapoint = torch.sum(log_w_matrix * ws_norm, 1)
 
         if model_type in ["general_alpha", "vralpha"] and not test:
-            ws_sum_per_datapoint /= (1 - self.alpha)
+            # For both VR-alpha and directly estimating L_alpha with a sum, we have to renormalize the sum with 1-alpha
+            ws_sum_per_datapoint /= (1 - alpha)
+
         loss = -torch.sum(ws_sum_per_datapoint)
 
         return loss
 
 # Compute N(obs| mu, sigma) for all K samples and sum over probabilities of the K samples
 def compute_log_probabitility_gaussian(obs, mu, logstd, axis=1):
-    # leaving out constant factor related to 2 pi in formula
     return torch.sum(-0.5 * ((obs-mu) / torch.exp(logstd)) ** 2 - logstd, axis)-.5*obs.shape[1]*T.log(torch.tensor(2*np.pi))
 
 # Compute Ber(obs| theta) for all K samples and sum over probabilities of the K samples
