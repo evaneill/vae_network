4a9a8642e4a922fb58835bdf764bec5ce49f75e0
ArneGeb <35544567+ArneGeb@users.noreply.github.com>
2020-04-29 13:01:02 +0200

    Fashion experiments that were never ran

--- experiments/our methods/fashion/vralpha.5_fashion_L2_K5_M128/likelihoods.py
+++
@@ -1,6 +0,0 @@
-def compute_log_probabitility_gaussian(obs, mu, logstd, axis=1):
-    # leaving out constant factor related to 2 pi in formula
-    return torch.sum(-0.5 * ((obs-mu) / torch.exp(logstd)) ** 2 - logstd, axis)-.5*obs.shape[1]*T.log(torch.tensor(2*np.pi))
-
-def compute_log_probabitility_bernoulli(obs, p, axis=1):
-    return torch.sum(p*torch.log(obs) + (1-p)*torch.log(1-obs), axis)
